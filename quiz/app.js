75612128f2ca43f739dd3fd1816c088723d3587a
ayesharajput <143730428+ayesharajput@users.noreply.github.com>
2023-10-23 20:02:33 +0500

    Add files via upload

---
+++ quiz/app.js
@@ -0,0 +1,12 @@
+var obj = {
+    name : "ayesha",
+    roll : "012",
+    Department :"biotechnolgy",
+    Marks:{
+        Biochemistry : 79,
+        BioMoleclues : 69,
+        Bio : 99,
+    }
+
+}
+console.log(obj)
